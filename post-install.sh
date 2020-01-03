af1703b071675d3888d7c8968cc1e25b2aa09568
jesgs <2304490+jesgs@users.noreply.github.com>
2020-01-02 19:22:45 -0600

    Removing shell script

--- post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp index.php ../../../index.php
-cp .env.example ../../../.env.example
-cp php.ini ../../../php.ini
-cp phpinfo.php ../../../phpinfo.php
-cp wp-config.php ../../../wp-config.php
