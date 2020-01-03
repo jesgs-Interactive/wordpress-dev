c64c9d502bbe15383e16fbf434d68fd2a3d775e9
jesgs <2304490+jesgs@users.noreply.github.com>
2020-01-02 20:00:46 -0600

    Re-adding shell script

---
+++ post-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp index.php ../../../index.php
+cp .env.example ../../../.env.example
+cp php.ini ../../../php.ini
+cp phpinfo.php ../../../phpinfo.php
+cp wp-config.php ../../../wp-config.php
